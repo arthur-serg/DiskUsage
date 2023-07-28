c209fa2bc4e709bd244615aeb0d0e06c6667bbc9
exponenta.euler <exponenta.euler@gmail.com>
2023-07-28 19:28:41 +0500

    usage

--- DiskUsage.cpp
+++ DiskUsage.cpp
@@ -1,7 +1,9 @@
-﻿#include "ArgsParser.hpp"
-
+﻿#include "DirectoryManager.hpp"
 
 int main(int argc, char* argv[])
 {
     auto parser = ArgsParser(argc, argv);
+    auto dirManager = DirectoryManager(parser);
+
+    dirManager.printInfo();
 }
