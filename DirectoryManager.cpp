8e85b38f7c9eec5369b10f29b318fd438ab95cd6
exponenta.euler <exponenta.euler@gmail.com>
2023-08-01 17:01:24 +0500

    fix for "-b" flag

--- DirectoryManager.cpp
+++ DirectoryManager.cpp
@@ -21,7 +21,6 @@ size_t DirectoryManager::getFileSize(const std::filesystem::path& path) const
     if (std::filesystem::is_regular_file(path))
     {
         size_t size = std::filesystem::file_size(path);
-        std::cout << "File: " << path.string() << ", Size: " << size << " bytes" << std::endl;
         return size;
     }
     if (std::filesystem::is_directory(path))
@@ -32,7 +31,6 @@ size_t DirectoryManager::getFileSize(const std::filesystem::path& path) const
             if (std::filesystem::is_regular_file(item.path()))
             {
                 size_t fileSize = std::filesystem::file_size(item.path());
-                std::cout << "File: " << item.path().string() << ", Size: " << fileSize << " bytes" << std::endl;
                 size += fileSize;
             }
             else if (std::filesystem::is_directory(item.path()))
@@ -61,7 +59,6 @@ size_t DirectoryManager::lookupDir(const std::filesystem::path& path, int depth)
             if (std::filesystem::is_directory(path))
             {
                 ThreadPool threadPool(std::thread::hardware_concurrency());
-                std::vector<std::pair<size_t, std::string>> fileSizes;
                 for (const auto& item : std::filesystem::directory_iterator(path))
                 {
                     if (std::filesystem::is_directory(item.path()))
@@ -72,27 +69,21 @@ size_t DirectoryManager::lookupDir(const std::filesystem::path& path, int depth)
                         });
                     }
 
-                    if (argsParser.needPrintEachFileInfo() && !std::filesystem::is_directory(item.path()))
+                    if (argsParser.needPrintEachFileInfo() || !std::filesystem::is_directory(item.path()))
                     {
                         if (argsParser.needPrintInBytes())
                         {
                             size_t size = getFileSize(item.path());
                             std::cout << size << "\t" << item.path().string() << std::endl;
-                            fileSizes.emplace_back(size, item.path().string());
                             totalSize += size;
                         }
                         else
                         {
                             size_t size = getFileSize(item.path()) / blockSizeInBytes;
                             std::cout << size << "\t" << item.path().string() << std::endl;
-                            fileSizes.emplace_back(size, item.path().string());
                             totalSize += size;
                         }
                     }
-                }
-                for (const auto& fileInfo : fileSizes)
-                {
-                    std::cout << fileInfo.first << "\t" << fileInfo.second << std::endl;
                 }
             }
         }
--- DiskUsage.cpp
+++ DiskUsage.cpp
@@ -2,8 +2,9 @@
 
 int main(int argc, char* argv[])
 {
-    auto test = "-b D:\\FinchRepos\\DiskUsage\\x64\\Release";
-    auto parser = ArgsParser(test);
+    /*auto test = "-b D:\\FinchRepos\\DiskUsage\\x64\\Release";
+    auto parser = ArgsParser(test);*/
+    auto parser = ArgsParser(argc, argv);
     auto dirManager = DirectoryManager(parser);
 
     dirManager.printInfo();
